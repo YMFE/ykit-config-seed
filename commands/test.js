bcbf12faed66e6fa3b34fe31c3fa1749b6a1eae2
yuhao.ju <yuhao.ju@qunar.com>
2017-05-03 19:50:31 +0800

    fix command test

--- commands/test.js
+++ commands/test.js
@@ -1,27 +1,16 @@
 'use strict'
 
-var path = require('path');
-var fs = require('fs-extra');
-
-exports.usage = '初始化项目环境';
+exports.usage = '<命令描述>';
 
 exports.setOptions = function(optimist) {
     // 如果需要支持命令参数，写成如下形式
-    optimist.alias('t', 'test');
-    optimist.describe('t', '<配置项描述>');
+    optimist.alias('f', 'foobar');
+    optimist.describe('f', '<配置项描述>');
 };
 
 exports.run = function(options) {
-    // let testParam = options.t || options.test
-    // 当执行 ykit <命令> -t 或 ykit <命令> --test 时，testParam 为 true，后续可根据参数执行更多操作
-
-    // 初始化脚手架
-    var cwd = process.cwd();
-    var boilerplatePath = path.join(__dirname, '../boilerplate/');
-
-    fs.copySync(path.join(boilerplatePath + '/src/'), path.join(cwd + '/src/'));
-    fs.copySync(path.join(boilerplatePath + '/index.html'), path.join(cwd + '/index.html'));
-    fs.copySync(path.join(boilerplatePath + '/ykit.seed.js'), path.join(cwd + '/ykit.seed.js'));
-
-    console.log('setup finished.');
+    // 当执行 ykit test -f 或 ykit test --foobar 时，foobar 为 true
+    var foobar = options.f || options.foobar
+    console.log('foobar:', foobar);
+    console.log('Finished ykit test.');
 };
